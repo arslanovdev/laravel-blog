eadb406a9dd8b3f9b1ce764ab6d2ec21e46abe28
Albert Arslanov <leon4ig2012@gmail.com>
2020-12-27 11:59:37 +0500

    authentication and layouts out of the box

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function () {
     Route::resource('posts', 'PostController')->names('blog.posts');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
