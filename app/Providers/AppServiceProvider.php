b124f29c806d52a87555b133ee54657bd172975d
Albert Arslanov <leon4ig2012@gmail.com>
2021-01-20 22:19:05 +0500

    add a forgotten BlogPost use

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Observers\BlogPostObserver;
+use App\Models\BlogPost;
 
 class AppServiceProvider extends ServiceProvider
 {
